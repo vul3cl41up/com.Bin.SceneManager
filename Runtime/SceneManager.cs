78679acb2cb99f91e4a827ca88c037e671ac372e
vul3cl41up <138540058+vul3cl41up@users.noreply.github.com>
2023-08-15 15:22:27 +0800

    Initial commit

---
+++ Runtime/SceneManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Bin.SceneManager
+{
+    /// <summary>
+    /// 場景管理器：切換場景與關閉遊戲
+    /// </summary>
+    public class SceneManager : MonoBehaviour
+    {
+        [SerializeField, Range(0.3f, 3)]
+        private float SoundDuration = 0.8f;
+
+        private int SceneIndex;
+
+        public void ChangeScene(int SceneIndex)
+        {
+            this.SceneIndex = SceneIndex;
+            Invoke("DelayChangeScene", SoundDuration);
+        }
+
+        public void DelayChangeScene()
+        {
+            print("切換場景");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneIndex);
+        }
+
+
+        public void Quit()
+        {
+            Invoke("DelayQuit", SoundDuration);
+        }
+
+        private void DelayQuit()
+        {
+            print("關閉遊戲");
+            Application.Quit();
+        }
+    }
+
+}
